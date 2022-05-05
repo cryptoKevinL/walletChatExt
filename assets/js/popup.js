36be9ccd313c08bec795b11adfe98d1ee37297a3
cyberKevin <>
2022-05-05 16:51:37 -0700

    added some connection to REST API from extension send button

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -2,6 +2,194 @@ const text = document.getElementById( 'notify-text' );
 const notify = document.getElementById( 'notify-button' );
 const reset = document.getElementById( 'notify-reset' );
 const counter = document.getElementById( 'notify-count' );
+//start of SimpleChat code
+let restApiUrl = 'https://simplechatapiforus.herokuapp.com/users'; 
+let streamID = 'this should never work'; // test data
+
+let selectedWalletAddress = "none";
+let loadedMsgs = []; 
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   console.log('DOMContent.........');
+//   window.ethereum.request({
+//     method: "eth_requestAccounts"
+//   });
+//   const interval = setInterval(function () {
+//     updateChatData();
+//   }, 5000);
+// });
+
+const fetchPost = data => {
+  fetch(` ${restApiUrl}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }).then(response => response.json()) //Then with the data from the response in JSON...
+  .then(data => {
+    console.log('$$$kl - Post to REST API:', data);
+  }) //Then with the error genereted...
+  .catch(error => {
+    console.error('Post to REST API error!!!!!!!!!!!!:', error);
+  });
+};
+
+const fetchPut = (data, id) => {
+  fetch(` ${restApiUrl}/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }).then(response => response.json()) //Then with the data from the response in JSON...
+  .then(data => {
+    console.log('$$$kl - PUT to REST API:', data);
+  }) //Then with the error genereted...
+  .catch(error => {
+    console.error('PUT to REST API error!!!!!!!!!!!!:', error);
+  });
+};
+
+const updateStreamID = resp => {
+  streamID = resp;
+  console.log('Message sending to REST API: ', streamID);
+  selectedWalletAddress =  "0xtestdude" //window.ethereum.selectedAddress; //Obj of data to send in future like a dummyDb
+
+  const sendToAddress = "0xextensiontest"; //msgerSendTo.value;
+  const commonName = "chromeExt"; //msgerMyName.value;
+  const data = {
+    streamID: `${streamID}`,
+    fromName: `${commonName}`,
+    fromAddr: `${selectedWalletAddress}`,
+    toAddr: `${sendToAddress}`,
+    read: false
+  }; //POST request with body equal on data in JSON format
+
+  fetchPost(data);
+};
+
+function addMessageReceiver(message, fromName, restApiMsgId) {
+  // if(message === "FALSE"){
+  //   console.log("updating message: ", message)
+  //   message = "<MSG UNSENT>" //signal that the sender undsent the original message by removing decryption permissions
+  // }
+  //const delay = message.split(" ").length * 100;
+  //setTimeout(() => {
+  if (!loadedMsgs[restApiMsgId]) {
+    console.log("adding message rx:", message);
+    appendMessage(fromName, BOT_IMG, "left", message);
+    loadedMsgs[restApiMsgId] = true;
+  } //}, delay);
+
+}
+
+function addMessageSender(message, fromName, wasRead, restApiMsgId) {
+  // if(wasRead == true)
+  //   textspan.appendChild(document.createTextNode(`${message}` + " (READ) (msgId:" + `${restApiMsgId}` + ")"));
+  // else if(wasRead == "unsent")
+  //   textspan.appendChild(document.createTextNode(`${message}` + " (UNSENT) (msgId:" + `${restApiMsgId}` + ")"));
+  // else
+  //   textspan.appendChild(document.createTextNode(`${message}` + " (UNREAD) (msgId:" + `${restApiMsgId}` + ")"));
+  if (!loadedMsgs[restApiMsgId]) {
+    console.log("adding message sender:", message);
+    appendMessage(PERSON_NAME, PERSON_IMG, "right", message);
+    loadedMsgs[restApiMsgId] = true;
+  }
+}
+
+function updateChatData() {
+  //GET request to get off-chain data for RX user
+  fetch(` ${restApiUrl}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(response => response.json()) //Then with the data from the response in JSON...
+  .then(data => {
+    //console.log('$$$kl - GET to REST API:', data);
+    // @ts-ignore
+    //const test = document.getElementById('sendaddr').value;
+    for (let i = 0; i < data.length; i++) {
+      //console.log("processing id: ", data[i].id)
+      const streamToDecrypt = data[i].streamID;
+
+      if (data[i].toAddr.toLowerCase() == selectedWalletAddress.toLowerCase()) {
+        addMessageReceiver(data[i].streamID, data[i].fromName, data[i].id); //mark as read if box is checked
+        // if(document.getElementById('readReceipts').checked && data[i].read != "unsent") {
+        //   console.log('$$$kl - marking READ for streamID: ', streamToDecrypt)
+        //   const putData = { streamID: `${data[i].streamID}`, fromName: `${data[i].fromName}`, fromAddr: `${data[i].fromAddr}`, toAddr: `${data[i].toAddr}`, read: true }
+        //   fetchPut(putData, data[i].id)
+        // }
+        // else {
+        //   console.log('$$$kl - read receipts is not checked, going rogue')
+        // }
+      } //print sent messages
+      else if (data[i].fromAddr.toLowerCase() == selectedWalletAddress.toLowerCase()) {
+        addMessageSender(data[i].streamID + "\n", data[i].fromName, data[i].read, data[i].id);
+      }
+    }
+  }) //Then with the error genereted...
+  .catch(error => {
+    console.error('GET to REST API error!!!!!!!!!!!!:', error);
+  });
+} //end LitChat copied functions
+
+
+// msgerForm.addEventListener("submit", event => {
+//   event.preventDefault();
+//   const msgText = msgerInput.value;
+//   if (!msgText) return; // appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
+
+//   msgerInput.value = ""; //send to REST API for storage
+
+//   updateStreamID(msgText); //TODO: need a better way to update more incrementally.
+
+//   updateChatData();
+// });
+
+function appendMessage(name, img, side, text) {
+  //   Simple solution for small apps
+  const msgHTML = `
+    <div class="msg ${side}-msg">
+      <div class="msg-img" style="background-image: url(${img})"></div>
+
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">${name}</div>
+          <div class="msg-info-time">${formatDate(new Date())}</div>
+        </div>
+
+        <div class="msg-text">${text}</div>
+      </div>
+    </div>
+  `;
+  msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+  msgerChat.scrollTop += 500;
+} // function botResponse() {
+//   const r = random(0, BOT_MSGS.length - 1);
+//   const msgText = BOT_MSGS[r];
+//   const delay = msgText.split(" ").length * 100;
+//   setTimeout(() => {
+//     appendMessage(BOT_NAME, BOT_IMG, "left", msgText);
+//   }, delay);
+// }
+// Utils
+
+function get(selector, root = document) {
+  return root.querySelector(selector);
+}
+
+function formatDate(date) {
+  const h = "0" + date.getHours();
+  const m = "0" + date.getMinutes();
+  return `${h.slice(-2)}:${m.slice(-2)}`;
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+//end of SimpleChat code
 
 chrome.storage.local.get( ['notifyCount'], data => {
 	let value = data.notifyCount || 0;
@@ -21,5 +209,7 @@ reset.addEventListener( 'click', () => {
 } );
 
 notify.addEventListener( 'click', () => {
-      text.value = 'you changed it';
+      //text.value = 'you changed it';
+
+	  updateStreamID(text.value);
 } );
